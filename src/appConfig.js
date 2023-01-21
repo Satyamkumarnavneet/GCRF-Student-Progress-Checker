99ef26106e24636205fe4c9569b317c7f46fc4ab
Satyamkumarnavneet <navneetsatyamkumar@gmail.com>
2023-01-22 00:45:45 +0530

    commit

--- src/appConfig.js
+++ src/appConfig.js
@@ -1,7 +1,3 @@
-// Each time you update change to the latest
-// date of updating. Also change your facilitator
-// instituion name. Change here will affect whole application.
-
 let facilitatorInstituionName = "Chandigarh University"
 let facilitatorLastUpdated = "13th MAY 2022"
 
